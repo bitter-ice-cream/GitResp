7a8e9f3cbc5272a3fdaa056d4d89c137dd9518da
luzl <1106424651@qq.com>
2022-05-31 16:40:38 +0800

    第一次提交

---
+++ untitled/src/com/lzl/java/learn/HelloIdea.java
@@ -0,0 +1,9 @@
+package com.lzl.java.learn;
+
+public class HelloIdea {
+    public static void main(String[] args) {
+        System.out.println(666);
+    }
+
+
+}
---
+++ untitled/src/com/lzl/java/learn/test1.java
@@ -0,0 +1,5 @@
+package com.lzl.java.learn;
+
+public class test1 {
+
+}

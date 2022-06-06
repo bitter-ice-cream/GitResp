527910d0308e113f9534374f5275e0ced35d3e89
luzl <1106424651@qq.com>
2022-06-06 13:14:16 +0800

    javaweb1

--- untitled/src/com/lzl/java/learn/Person.java
+++ untitled/src/com/lzl/java/learn/Person.java
@@ -3,4 +3,8 @@ package com.lzl.java.learn;
 public class Person {
     private String name;
     private int age;
+    public static void main(String[] args) {
+        System.out.println(666);
+
+    }
 }
